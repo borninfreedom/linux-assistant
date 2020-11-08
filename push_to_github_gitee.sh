28a61f5ea5c28f3f8807631a032381bfadd9764e
borninfreedom <z19040042@s.upc.edu.cn>
2020-11-08 15:48:18 +0800

    2020年 11月 08日 星期日 15:48:18 CST

--- push_to_github_gitee.sh
+++ push_to_github_gitee.sh
@@ -1,3 +1,6 @@
+git remote add gitee https://gitee.com/borninfreedom/linux-assistant
+git remote add github https://github.com/borninfreedom/linux-assistant
+
 git add .
 git commit -m "$(date)"
 git push gitee master
